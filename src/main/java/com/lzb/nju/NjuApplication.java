6737b4fc47858475ad927ff319a5308cff3fd552
161250063 <161250063@smail.nju.edu.cn>
2018-04-12 11:00:25 +0800

    initialize

---
+++ src/main/java/com/lzb/nju/NjuApplication.java
@@ -0,0 +1,12 @@
+package com.lzb.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjuApplication.class, args);
+	}
+}
