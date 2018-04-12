4914c1ab7837e3bc049fc92299742779690b903a
161250063 <161250063@smail.nju.edu.cn>
2018-04-12 11:05:32 +0800

    add source code and test

---
+++ src/main/java/com/lzb/nju/Hello.java
@@ -0,0 +1,13 @@
+package com.lzb.nju;
+
+/**
+ * @author lzb
+ * @date 2018/4/12 11:04
+ */
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("hello github ci");
+    }
+
+}
---
+++ src/test/java/com/lzb/nju/HelloTest.java
@@ -0,0 +1,17 @@
+package com.lzb.nju;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author lzb
+ * @date 2018/4/12 11:05
+ */
+public class HelloTest {
+
+    @Test
+    public void main() {
+        assert(true);
+    }
+}
